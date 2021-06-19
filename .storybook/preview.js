17011121fc841a51d0d55a2fdf136aafa68a7a74
Sangram Mukherjee <sangram.mukherjee@outlook.com>
2021-06-20 00:41:43 +0530

    UPDATE: Update documentation and npm related changes to storybook with npm version > 7.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,4 +2,10 @@ import './storybook.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
 }
